bfaf40f7ffb43d9f9c2cf1c43a34c4ded9849a9c
Rodrigo Valente <rodrigolvalente@gmail.com>
2020-07-11 00:37:17 -0300

    change api url

--- src/http-common.js
+++ src/http-common.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:3000',
-  //baseURL: 'https://my-grades-api-igti.herokuapp.com/',
+  baseURL: 'https://my-grades-api-igti.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
